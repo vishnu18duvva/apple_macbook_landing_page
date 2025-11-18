ed62a0f61a244ec74aa07929e93453ad123d7769
vishnu18duvva <duvva1804@gmail.com>
2025-11-18 22:45:51 +0530

    Initial Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { navLinks } from '../constants'
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav>
+                <img src="/logo.svg" alt="Apple Logo" className="w-8 h-8" />
+
+                <ul>
+                    {navLinks.map(({label}) => (
+                        <li key={label}>
+                            <a href={label}>{label}</a>
+                        </li>
+                    ))}
+                </ul>
+
+
+                <div>
+                    <button>
+                        <img src="/search.svg" alt="Search" />
+                    </button>
+                    <button>
+                        <img src="/cart.svg" alt="Cart" />
+                    </button>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
