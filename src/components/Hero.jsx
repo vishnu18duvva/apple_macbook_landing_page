dcd3914454d5b06b42aadb8b614c4ccfb1bbd54f
vishnu18duvva <duvva1804@gmail.com>
2025-11-19 15:06:40 +0530

    ShowCase Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,25 @@
-import React, { useEffect, useRef } from 'react'
+import {useEffect, useRef} from "react";
 
 const Hero = () => {
-  const videoRef = useRef();
+    const videoRef = useRef();
 
-  useEffect(() => {
-    if (videoRef.current) videoRef.current.playbackRate= 2.0;
+    useEffect(() => {
+        if(videoRef.current) videoRef.current.playbackRate = 2;
     }, []);
 
-    
-  return (
-    <section id="hero">
-        <div>
-            <h1>MacBook Pro</h1>
-            <img src='/title.png' alt='MacBook Title' />
-        </div>
+    return (
+        <section id="hero">
+            <div>
+                <h1>MacBook Pro</h1>
+                <img src="/title.png" alt="MacBook Title" />
+            </div>
 
-        <video ref={videoRef} src ='/videos/hero.mp4' autoPlay muted playsInline />
+            <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline />
 
-        <button>Buy</button>
+            <button>Buy</button>
 
-        <p>From $2999 or $1559/mo. for 24 months</p>
-    </section>
-  )
-  
+            <p>From $1599 or $133/mo for 12 months</p>
+        </section>
+    )
 }
-
 export default Hero
